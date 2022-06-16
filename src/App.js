3b71aa2c5809782cb8fd8440f69f823c6fe3dceb
Fang Lu Lim <fanglulim96@gmail.com>
2022-06-15 20:48:58 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled, { ThemeProvider } from "styled-components";
+import "./App.css";
+import theme from "styled-theming";
+import { Provider as ReduxProvider } from "react-redux";
+import store from "./redux/store";
+import DarkThemeProvider from "./DarkThemeProvider";
+import DarkThemeToggle from "./DarkThemeToggle";
+
+export const theme1 = theme("theme", {
+  light: "#fff",
+  dark: "#2d2d2d",
+});
+
+export const theme2 = theme("theme", {
+  light: "#000",
+  dark: "#fff",
+});
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+  font-family: sans-serif;
+  background-color: ${theme1};
+  color: ${theme2}
+`;
+
+const SwitchTheme = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 50px;
+  height: 75px;
+  background-color: ${theme2};
+  color: ${theme1};
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <ReduxProvider store={store}>
+        <DarkThemeProvider>
+          <SwitchTheme>
+            <h1>Theme App</h1>
+            <p>
+              <DarkThemeToggle />
+            </p>
+          </SwitchTheme>
+
+          <Container>
+            <h2>Welcome!</h2>
+            <h3>Full Stack Web Development</h3>
+          </Container>
+          
+        </DarkThemeProvider>
+      </ReduxProvider>
+    </React.Fragment>
   );
 }
 
